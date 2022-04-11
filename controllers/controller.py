f243bc9466c99941f92270f6d0ddca132ff5a5af
Abi Johnson <99146064+AbiJays@users.noreply.github.com>
2022-04-11 09:09:52 +0100

    added events app homework

---
+++ controllers/controller.py
@@ -0,0 +1,12 @@
+from flask import render_template
+
+from app import app
+from models.event_list import events # get a hold of all the tasks
+
+# change the template to display all events
+
+@app.route('/events')
+def index():
+    # print(events) if you write this then refresh the web browser page it prints in terinal. same with breakpoint()
+    return render_template("index.html", title="Abi", all_events = events) # pass the tasks down to the template
+    # events in line 4 and events in line 11 must be the same. all_events must be how it is refered to as such in template.
---
+++ models/event_list.py
@@ -0,0 +1,8 @@
+from models.events import *
+
+event1 = Event("15/4/2022", "Becky & Luke", 300, 1, "Travers-Sneader wedding")
+event2 = Event("16/5/2022", "Kirstin & Jon", 590, 2, "Lavison wedding")
+event3 = Event("18/6/2022", "Neveen & Alice", 450, 3, "Aleen wedding")
+event4 = Event("15/7/2022", "Ben & Kirsty", 120, 4, "Birst wedding")
+event5 = Event("20/8/2022", "Rania & Tom", 290, 5, "Kamal-Hughes wedding")
+events = [event1, event2, event3, event4, event5]
