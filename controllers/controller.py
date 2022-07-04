432e38a46e2578f1921aeb2d9d56cc326b133f2d
Abi Johnson <99146064+AbiJays@users.noreply.github.com>
2022-07-04 13:34:26 +0100

    finished

--- controllers/controller.py
+++ controllers/controller.py
@@ -10,3 +10,5 @@ def index():
     # print(events) if you write this then refresh the web browser page it prints in terinal. same with breakpoint()
     return render_template("index.html", title="Abi", all_events = events) # pass the tasks down to the template
     # events in line 4 and events in line 11 must be the same. all_events must be how it is refered to as such in template.
+
+# POST ACTION
